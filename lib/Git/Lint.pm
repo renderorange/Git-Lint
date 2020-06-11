4fb921923c1fb90f9eec0780c785516610aac31d
Blaine Motsinger <blaine@renderorange.com>
2020-06-11 00:19:08 -0500

    Add config method and pod to Git::Lint

--- lib/Git/Lint.pm
+++ lib/Git/Lint.pm
@@ -22,12 +22,20 @@ sub new {
     return $self;
 }
 
+sub config {
+    my $self = shift;
+    return $self->{_config};
+}
+
 sub run {
     my $self = shift;
     my $opt  = shift;
 
+    die 'git-lint: profile ' . $opt->{profile} . ' was not found' . "\n"
+        unless exists $self->config->{profiles}{ $opt->{profile} };
+
     my @issues;
-    foreach my $check ( @{ $self->{_config}{profiles}{ $opt->{profile} } } ) {
+    foreach my $check ( @{ $self->config->{profiles}{ $opt->{profile} } } ) {
         my $class = 'Git::Lint::Check::Commit::' . $check;
         try {
             $self->{_loader}->load($class);
@@ -52,3 +60,76 @@ sub run {
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Git::Lint - linter for git
+
+=head1 SYNOPSIS
+
+ use Git::Lint;
+
+ my $lint = Git::Lint->new();
+ $lint->run({ profile => 'default' });
+
+=head1 DESCRIPTION
+
+C<Git::Lint> is a pluggable lint framework for git, written in Perl.
+
+=head1 CONSTRUCTOR
+
+=over
+
+=item new
+
+Returns a reference to a new C<Git::Lint> object.
+
+=back
+
+=head1 METHODS
+
+=over
+
+=item run
+
+Loads the check modules as defined by C<profile>.
+
+C<run> accepts the following arguments:
+
+B<profile>
+
+The name of a defined set of check modules to run.
+
+=item config
+
+Returns the C<Git::Lint::Config> object created by C<Git::Lint>.
+
+=back
+
+=head1 CONFIGURATION
+
+Configuration is done through C<git config> files (F<~/.gitconfig> or F</repo/.git/config>).
+
+Only one profile, C<default>, is defined internally. C<default> contains all check modules by default.
+
+The C<default> profile can be overridden through C<git config>.
+
+To set the default profile to only run the C<Whitespace> check:
+
+ [lint "profiles"]
+     default = Whitespace
+
+Or set the default profile to C<Whitespace> and the fictional check, C<Flipdoozler>:
+
+ [lint "profiles"]
+     default = Whitespace, Flipdoozler
+
+=head1 AUTHOR
+
+Blaine Motsinger C<blaine@renderorange.com>
+
+=cut
