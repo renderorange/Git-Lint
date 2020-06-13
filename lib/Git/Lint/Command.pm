0250ad20faf977ec810519010085d6941bd7ae5e
Blaine Motsinger <blaine@renderorange.com>
2020-06-13 11:48:14 -0500

    Add pod for Command.pm

--- lib/Git/Lint/Command.pm
+++ lib/Git/Lint/Command.pm
@@ -23,3 +23,36 @@ sub run {
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Git::Lint::Command - run commands
+
+=head1 SYNOPSIS
+
+ use Git::Lint::Command;
+ my $config_raw = Git::Lint::Command::run(\@git_config_cmd);
+
+=head1 DESCRIPTION
+
+C<Git::Lint::Command> runs commands and returns output.
+
+=head1 SUBROUTINES
+
+=over
+
+=item run
+
+Runs the passed command and returns the output.
+
+=back
+
+=head1 AUTHOR
+
+Blaine Motsinger C<blaine@renderorange.com>
+
+=cut
