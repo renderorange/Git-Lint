5a6aad49b7bd7b7631cad0582983bd9eaf9c3da6
Blaine Motsinger <blaine@renderorange.com>
2024-01-02 14:43:44 -0600

    Parse config for localdir and load into config
    This commit updates the load and user_config methods to support
    parsing git config for the lint.config.localdir setting, then load
    it into the returned config object and add that directory into INC.
    
    This commit also fixes a bug in the regex for parsing the user
    profiles.

--- lib/Git/Lint/Config.pm
+++ lib/Git/Lint/Config.pm
@@ -16,6 +16,25 @@ sub load {
     $self->{profiles}{commit}{default}  = [];
     $self->{profiles}{message}{default} = [];
 
+    bless $self, $class;
+
+    my $user_config = $self->user_config();
+
+    # add user defined config settings
+    if ( exists $user_config->{config} ) {
+        $self->{config} = $user_config->{config};
+    }
+
+    # if localdir is defined, add it into INC to load modules from
+    if ( exists $self->{config} && defined $self->{config}{localdir} ) {
+        if ( -d $self->{config}{localdir} ) {
+            push @INC, $self->{config}{localdir};
+        }
+        else {
+            warn "git-lint: [warn] configured localdir $self->{config}{localdir} isn't a readable directory\n";
+        }
+    }
+
     # all check modules are added to the default profile
     my $loader        = Module::Loader->new;
     my $namespace     = 'Git::Lint::Check::Commit';
@@ -32,16 +51,10 @@ sub load {
         $self->{profiles}{message}{default} = \@message_checks;
     }
 
-    bless $self, $class;
-
-    my $user_config = $self->user_config();
-
     # user defined profiles override internally defined profiles
-    foreach my $cat ( keys %{$user_config} ) {
-        foreach my $check ( keys %{ $user_config->{$cat} } ) {
-            foreach my $profile ( keys %{ $user_config->{$cat}{$check} } ) {
-                $self->{$cat}{$check}{$profile} = $user_config->{$cat}{$check}{$profile};
-            }
+    foreach my $check ( keys %{ $user_config->{profiles} } ) {
+        foreach my $profile ( keys %{ $user_config->{profiles}{$check} } ) {
+            $self->{profiles}{$check}{$profile} = $user_config->{profiles}{$check}{$profile};
         }
     }
 
@@ -67,12 +80,22 @@ sub user_config {
     }
 
     my %parsed_config = ();
+
+    # load check profiles
     foreach my $line ( split( /\n/, $stdout ) ) {
-        next unless $line =~ /^lint\.(\w+).(\w+).(\w+)\s+(.+)$/;
-        my ( $cat, $check, $profile, $value ) = ( $1, $2, $3, $4 );
+        next unless $line =~ /^lint\.profiles\.(\w+)\.(\w+)\s+(.+)$/;
+        my ( $check, $profile, $value ) = ( $1, $2, $3 );
 
         my @values = List::MoreUtils::apply {s/^\s+|\s+$//g} split( /,/, $value );
-        push @{ $parsed_config{$cat}{$check}{$profile} }, @values;
+        push @{ $parsed_config{profiles}{$check}{$profile} }, @values;
+    }
+
+    # load other config settings
+    foreach my $line ( split( /\n/, $stdout ) ) {
+        if ( $line =~ /^lint\.config\.localdir\s+(.+)$/ ) {
+            my ($value) = ($1);
+            $parsed_config{config}{localdir} = $value;
+        }
     }
 
     return \%parsed_config;
